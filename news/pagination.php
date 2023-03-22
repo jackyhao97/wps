c1ca9f65cc20a83dd76e794cdaaa1d37c900e95d
Jacky <jackyhau97@gmail.com>
2023-03-22 17:25:24 +0700

    update news

--- news/pagination.php
+++ news/pagination.php
@@ -1,12 +1,13 @@
 <?php
   require_once '../config.php';
   // $connect = new PDO("mysql:host=localhost; dbname=wps", "root", "");
-  $limit = 12;
+  $limit = 6;
   $page = 1;
   // $jlhBeritaLainnya = 4; lIMIT JUMLAH BERITA SEHINGGA MEMUNGKINKAN BERITA MULAI DARI NO BERAPA
   $jlhBeritaLainnya = 0;
+  $postpage = isset($_POST["page"]) ? $_POST["page"] : 0;
   
-  if ($_POST["page"] > 1) {
+  if ($postpage > 1) {
     $start = (($_POST["page"] - 1)) * $limit;
     $page = $_POST['page'];
   }
@@ -41,15 +42,21 @@
     $date = strtotime($row["tgl_berita"]);
     $date = date("d-m-Y", $date);
     $output .=  '
-              <div class="col-12 col-sm-6 col-lg-4 mt-4">
+              <div class="col-12 mt-4">
                 <div class="card" style="overflow: hidden; cursor: pointer; box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.6);" onclick="window.location=`' .$row["seo_link"]. '`">
-                  <div class="card-header" style="border: 0; padding: 0; border-radius: 0; height: 200px; overflow: hidden">
-                    <img src="' .SITE_NEWS.$row["path"]. '" class="card-img-top" alt="'.$row["judul"].'">
-                  </div>
-                  <div class="card-body">
-                    <small class="card-text text-muted">' .$date. '</small>
-                    <p class="card-text" style="margin-bottom: 0; height: 100px">' .$row["judul"]. '</p>
-                    <a class="btn btn-danger selengkapnya-lain" href="./' .$row["seo_link"].'"><i class="fas fa-plus mr-2" style="font-size: 12px"></i>SELENGKAPNYA</a>                    
+                  <div class="row g-0">
+                    <div class="col-md-4">
+                      <div class="card-header" style="border: 0; padding: 0; border-radius: 0; height: 200px; overflow: hidden">
+                        <img src="' .SITE_NEWS.$row["path"]. '" class="card-img-top" alt="'.$row["judul"].'">
+                      </div>
+                    </div>
+                    <div class="col-md-8">
+                      <div class="card-body">
+                        <small class="card-text text-muted">' .$date. '</small>
+                        <p class="card-text" style="margin-bottom: 0; height: 100px">' .$row["judul"]. '</p>
+                        <a class="btn btn-danger selengkapnya-lain fw-bold" href="./' .$row["seo_link"].'"><i class="fas fa-plus me-2" style="font-size: 12px"></i>SELENGKAPNYA</a>                    
+                      </div>
+                    </div>  
                   </div>
                 </div>
               </div>
@@ -126,7 +133,7 @@
 
       $next_id = $page_array[$count] + 1;
 
-      if ($next_id >= $total_links) {
+      if ($next_id > $total_links) {
         $next_link =  '
                       <li class="page-item disabled">
                         <a class="page-link" href="#">&gt;</a>
