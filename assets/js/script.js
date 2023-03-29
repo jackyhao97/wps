43c02998fbf9a29c7e9fd096de763a957c153f85
Jacky Hao <56533122+jackyhao97@users.noreply.github.com>
2023-03-29 13:17:43 +0700

    change script

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,18 @@ $("#switchLang").click(function () {
   window.location =
     window.location.protocol + "//" + window.location.host + `/wps/id/`;
 });
+
+const swiperTestimonial = new Swiper(".swiper-container-testimonial", {
+  pagination: {
+    el: ".swiper-pagination-testimonial",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
